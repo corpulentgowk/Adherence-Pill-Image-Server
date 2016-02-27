52d3d25f08a43c613b7682906371c6530e6500eb
corpulentgowk <stevenzmanx@u.northwestern.edu>
2016-02-27 15:12:48 -0600

    model retrieval and insertion
    model retrieval, insertion, and image retrieval modified

--- app.py
+++ app.py
@@ -1,40 +1,48 @@
 from flask import Flask, request
-
+import ast
 from parse_rest.connection import register #Connects to the PArse Database. Format: <application_id>, <rest_api_key>[, master_key=None]
 register("HW8S7gMIafiQQszmJme2IS4Be7jFlRHnE0izdtLs", "D0lEeiwQ62X6POKXJ1RTxbHuDPX91aUvditAIjxC", master_key="fDtm8fpoHSxbeH3iUGaEexoRgsSdiBh2MvYGDjej")
 
+#File Handling
+import os
+from flask import Flask, request, redirect, url_for, jsonify
+from werkzeug import secure_filename
+import json,httplib
+
+UPLOAD_FOLDER = '/Users/Steven-PC/Desktop/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'bin'])
+
+
+#Parse handling
 from parse_rest.datatypes import Object
 
 from PIL import Image
 import urllib, cStringIO
 
 def imagesRetrieve():
-    class ImageStorageDev(Object):
-        pass
+   class ImageStorageDev(Object):
+       pass
 
-    #Get data from parse by objectId
-    #dataRow = ImageStorageDev.Query.get(objectId="uqA3Ycyqnq")
+   #Get data from parse by objectId
+   #dataRow = ImageStorageDev.Query.get(objectId="uqA3Ycyqnq")
 
-    imageDict = {}
-    dataRows = ImageStorageDev.Query.filter(processed=False)
-    for dataRow in dataRows:
-        #Get data column (image) for that record
-        image = dataRow.image
-        #Get image URL
-        imageURL = image.url
-        imageDict[dataRow.objectId] = imageURL
-    return imageDict
-
-def updateImages(result):
-    class ImageStorageDev(Object):
-        pass
-    for key in result:
-        dataRow = ImageStorageDev.Query.get(objectId=key)
-        dataRow.processed=True
-        print result[key]
-        dataRow.pill = result[key]
-        dataRow.save()
-
+   imageDict = {}
+   dataRows = ImageStorageDev.Query.filter(processed=False)
+   #print dataRows
+   for dataRow in dataRows:
+       #Get data column (image) for that record
+       image = dataRow.image
+       print type(image)
+       imageList = []
+       #Get image URL
+       imageURL = image.url
+       category = dataRow.category
+       if category not in imageDict:
+           imageList.append(imageURL)
+           imageDict[category] = imageList
+       else:
+          imageDict[category].append(imageURL)
+   return imageDict
 
 #Get image (We don't actually need the file, URL should be enough)
 #Get image from image URL
@@ -43,6 +51,8 @@ def updateImages(result):
 #pic.show()
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
 def hello_world():
     #pic = Image.open(image)
@@ -50,55 +60,86 @@ def hello_world():
     #picTag = '<img src="' + imageURL + '" alt="' + imageURL + '">'
     #print picTag
     #return "Hello World" + picTag
-    return "Hello World"
+    return "Welcome to The Adherence Pill Image Processing Server"
 
 #In the future, we may want to change this method to POST.
-@app.route('/update/<objectID>')
-def getResult(objectID):
-    pill = request.args.get('pill','')
-    result = {}
-    if pill is not None:
-        result[objectID] = pill
-        updateImages(result)
-        return 'ObjectID: {0}, Pill: {1}'.format(objectID,pill)
-    return 'Pill not found for ObjectID %s' % objectID
+# @app.route('/update/<objectID>')
+# def getResult(objectID):
+#     pill = request.args.get('pill','')
+#     result = {}
+#     if pill is not None:
+#         result[objectID] = pill
+#         updateImages(result)
+#         return 'ObjectID: {0}, Pill: {1}'.format(objectID,pill)
+#     return 'Pill not found for ObjectID %s' % objectID
 
-@app.route('/getData')
+@app.route('/getImages')
 def getData():
     dic = imagesRetrieve()
-    result = ""
-    # These results may look bad on browser, but by using telnet, it actually
-    # starts a new line.
-    for key, value in dic.iteritems():
-        result = result + key + ":" + value + "\n"
-    return result
+
+    return jsonify(dic)
 
 
 #Sample Request
 # r = requests.post('http://httpbin.org/post', files={'report.xls': open('report.xls', 'rb')})
 #r = requests.post('http://localhost:5000', files={'test.txt': open('test.txt', 'rb')})
-
-@app.route('/addModel/<param>', methods=['POST'])
-def insertModel(param):
-    param = str(param)
-    dat = ast.literal_eval(param)
-    dat = ast.literal_eval(dat)
+#r = requests.post('http://localhost:5000/addModel', files={'test.txt': open('test.txt', 'rb')})
+# r = requests.post("http://localhost:5000/addModel", files={'test.bin': open('test.bin', 'rb')})
+@app.route('/addModel', methods=['POST'])
+def insertModel():
 
     if request.method == 'POST':
-        file = request.files['file']
-    class ModelStorage(Object):
-        pass
-    modelStorage = ModelStorage(patientID=dat['patientID'], model=file)
+        for filename, file in request.files.iteritems():
+            name = request.files[filename].name
+    file1 = request.files[name] #Grabs the file that was uploaded
 
+#### Uploads File to Parse ####
+
+    connection = httplib.HTTPSConnection('api.parse.com', 443) #Will need to be changed later to wherever we offload parse api
+    connection.connect()
+    connection.request('POST', '/1/files/' + name, file1.read(),
+                   {
+           "X-Parse-Application-Id": "HW8S7gMIafiQQszmJme2IS4Be7jFlRHnE0izdtLs",
+           "X-Parse-REST-API-Key": "D0lEeiwQ62X6POKXJ1RTxbHuDPX91aUvditAIjxC",
+           "Content-Type": "application/octet-stream"
+         })
+
+    res = connection.getresponse().read()
+    res = str(res)
+    res = ast.literal_eval(res) #converts the response into a JSON so we can index it by strings
+    # res["name"] #Grab the link that parse stored in the files class so we can associate the file with ModelStorage
+
+#### Adds Recently Uploaded File to Parse to Class: Model Storage ####
+
+    connection.request('POST', '/1/classes/ModelStorage', json.dumps({
+       "model": {
+         "name": res["name"],
+         "__type": "File"
+       }
+     }), {
+       "X-Parse-Application-Id": "HW8S7gMIafiQQszmJme2IS4Be7jFlRHnE0izdtLs",
+       "X-Parse-REST-API-Key": "D0lEeiwQ62X6POKXJ1RTxbHuDPX91aUvditAIjxC",
+       "Content-Type": "application/json"
+     })
+    result = json.loads(connection.getresponse().read())
+    print result
     return "Successfully inserted model"
 
+@app.route('/getModels', methods=['GET'])
+def fetchModel():
+    class ModelStorage(Object):
+       pass
 
-@requires_auth
-def update_task(param):
-    param = str(param)
-    dat = ast.literal_eval(param)
-    dat = ast.literal_eval(dat)
-    cur.execute("select exists(select 1 from utiltrack where jobid='%s');" % (dat['jobid']))
+    modelDict = {}
+    dataRows =  ModelStorage.Query.all()
+    for dataRow in dataRows:
+       #Get data column (file) for that record
+       model = dataRow.model
+       id = dataRow.objectId
+       modelDict[id] = model.url
+
+    return jsonify(modelDict)
+
 if __name__ == '__main__':
     #app.debug = True
-    app.run()
+    app.run(debug = 'True')
