927d445191ab4fafafcc37583abc5e5c1912a7a7
corpulentgowk <stevenlayne2017@u.northwestern.edu>
2016-02-27 10:43:45 -0600

    Adding Model Insert

--- app.py
+++ app.py
@@ -1,26 +1,121 @@
-from flask import Flask
-
+from flask import Flask, request
+import ast
 from parse_rest.connection import register #Connects to the PArse Database. Format: <application_id>, <rest_api_key>[, master_key=None]
 register("HW8S7gMIafiQQszmJme2IS4Be7jFlRHnE0izdtLs", "D0lEeiwQ62X6POKXJ1RTxbHuDPX91aUvditAIjxC", master_key="fDtm8fpoHSxbeH3iUGaEexoRgsSdiBh2MvYGDjej")
 
+#File Handling
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/Users/Steven-PC/Desktop/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'bin'])
+
+
+
+
+
+
+#Parse handling
 from parse_rest.datatypes import Object
 
-class GameScore(Object): #Declare the class you want to access in the parse database
-    pass
-gameScore = GameScore(score=1337, player_name='John Doe', cheat_mode=False)
-gameScore.cheat_mode = True
-gameScore.level = 20
-gameScore.save()
+#from PIL import Image
+import urllib, cStringIO
+
+# def imagesRetrieve():
+#     class ImageStorageDev(Object):
+#         pass
+#
+#     #Get data from parse by objectId
+#     #dataRow = ImageStorageDev.Query.get(objectId="uqA3Ycyqnq")
+#
+#     imageDict = {}
+#     dataRows = ImageStorageDev.Query.filter(processed=False)
+#     for dataRow in dataRows:
+#         #Get data column (image) for that record
+#         image = dataRow.image
+#         #Get image URL
+#         imageURL = image.url
+#         imageDict[dataRow.objectId] = imageURL
+#     return imageDict
+#
+# def updateImages(result):
+#     class ImageStorageDev(Object):
+#         pass
+#     for key in result:
+#         dataRow = ImageStorageDev.Query.get(objectId=key)
+#         dataRow.processed=True
+#         print result[key]
+#         dataRow.pill = result[key]
+#         dataRow.save()
 
 
-gameScore = GameScore.Query.get(objectId="xxwXx9eOec")
+#Get image (We don't actually need the file, URL should be enough)
+#Get image from image URL
+#imageFile = cStringIO.StringIO(urllib.urlopen(imageURL).read())
+#pic = Image.open(imageFile)
+#pic.show()
+
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    #pic = Image.open(image)
+    #image.show()
+    #picTag = '<img src="' + imageURL + '" alt="' + imageURL + '">'
+    #print picTag
+    #return "Hello World" + picTag
+    return "Hello World"
 
+#In the future, we may want to change this method to POST.
+# @app.route('/update/<objectID>')
+# def getResult(objectID):
+#     pill = request.args.get('pill','')
+#     result = {}
+#     if pill is not None:
+#         result[objectID] = pill
+#         updateImages(result)
+#         return 'ObjectID: {0}, Pill: {1}'.format(objectID,pill)
+#     return 'Pill not found for ObjectID %s' % objectID
+
+# @app.route('/getData')
+# def getData():
+#     dic = imagesRetrieve()
+#     result = ""
+#     # These results may look bad on browser, but by using telnet, it actually
+#     # starts a new line.
+#     for key, value in dic.iteritems():
+#         result = result + key + ":" + value + "\n"
+#     return result
+
+
+#Sample Request
+# r = requests.post('http://httpbin.org/post', files={'report.xls': open('report.xls', 'rb')})
+#r = requests.post('http://localhost:5000', files={'test.txt': open('test.txt', 'rb')})
+
+@app.route('/addModel/<param>', methods=['POST'])
+def insertModel(param):
+    param = str(param)
+    dat = ast.literal_eval(param)
+
+    if request.method == 'POST':
+        file = request.files['test.bin']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        #print str(file)
+    class ModelStorage(Object):
+        pass
+
+    with open("/Users/Steven-PC/Desktop/Rplots.pdf", 'rb').read() as f:
+        modelStorage = ModelStorage(patientID=dat['patientID'], model=f)
+        temp = str(f)
+
+        print(f)
+        modelStorage.save()
+    return "Successfully inserted model"
 
 if __name__ == '__main__':
-    app.run()
+    #app.debug = True
+    app.run(debug = 'True')
